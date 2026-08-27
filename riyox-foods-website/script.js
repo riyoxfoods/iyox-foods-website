207621c0f164c504274934120c59bfe0ce0a6c6f
riyoxfoods <riyoxfoods@gmail.com>
2026-08-27 14:25:38 +0530

    Delete riyox-foods-website directory

--- riyox-foods-website/script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("year").textContent = new Date().getFullYear();
-const menu=document.querySelector(".menu");
-const nav=document.querySelector(".navlinks");
-menu?.addEventListener("click",()=>{nav.classList.toggle("mobile-open")});
